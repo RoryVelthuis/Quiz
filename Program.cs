1e42de09df1630152f089f2920750ae977318fca
PurpleSm0g <RorySVelthuis@gmail.com>
2022-09-20 11:05:25 +1200

    added code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace QuizGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] questions = { "How many elements are in the periodic table?" };
+            string[] answers = { "118" };
+            int score = 0;
+
+            do
+            {
+                for(int i = 0; i < questions.Length; i++)
+                {
+                    Console.WriteLine(questions[i]);
+                    string answer = Console.ReadLine();
+                    if(answer == answers[i])
+                    {
+                        Console.WriteLine("Correct");
+                        score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect");
+                    }
+                }
+            } while (true);
+
         }
     }
 }
